a43f79167184329f4e742c4defc34805eab07920
IslamSalimzhanov <94981808+Somikkk@users.noreply.github.com>
2023-09-15 09:52:15 +0600

    Add files via upload

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if(first > second) {
+            System.out.println(first);
+        } else {
+            System.out.println(second);
+        }
+    }
+
+    public static void main(String[] args) {
+        new GreatestNumberPrinter().printGreatest(-400, 1);
     }
 }
